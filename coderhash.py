190f726d4cd8c8a44b20d6217462ded7dc68e072
Ing. Danny Colmenares Pérez <dannycolmenarespdv@gmail.com>
2024-02-26 21:48:10 -0400

    mejoras en ventana
    mejoras en ventana

--- coderhash.py
+++ coderhash.py
@@ -1,10 +1,10 @@
 # Programa para codificar cadenas de texto en hash
 
-import tkinter as tk
+from tkinter import *
 #import hash256
 
 # Creacion de la ventana principal
-window = tk.Tk()
+window = Tk()
 
 # Titulo de la ventana
 window.title("Codificador HASH")
@@ -12,13 +12,24 @@ window.title("Codificador HASH")
 # Configurando dimensiones y ubicacion de la ventana
 window.geometry("600x400+360+140")
 
+window.minsize(400,200)
+window.maxsize(800,600)
+
+# Cambia el color de la ventana
+#window.configure(bg="lightblue")
+
 # Creacion de las etiquetas
-mensaje1 = tk.Label(window, text="Programa para codificar cadena de caracteres en hash256")
-mensaje2 = tk.Label(window, text="Ingrese una cadena de caracteres para codificarla")
+mensaje1 = Label(window, text="Programa para codificar cadena de caracteres en hash256")
+mensaje2 = Label(window, text="Ingrese una cadena de caracteres para codificarla")
+
+# Modificar la presentación de los mensajes
+mensaje1.config(font=("Arial", 14))
+mensaje2.config(font=("Arial", 12))
 
 # Mostrar etiquetas
 mensaje1.grid(row=0, column=0)
 mensaje2.grid(row=1, column=0)
-
+#mensaje1.pack()
+#mensaje2.pack()
 # Bucle de ejecucion de la ventana
 window.mainloop()
